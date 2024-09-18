f768176ed5e335e5acdbea5334a7a0b22f93fa3c
Florian Vaussard <florian.vaussard@gmail.com>
2024-09-18 06:45:59 +0200

    fvd: adding part1 (kernel intro)

---
+++ mk/linux-kernel-fvd-part1.mk
@@ -0,0 +1,11 @@
+LINUX_KERNEL_FVD_PART1_SLIDES = \
+		first-slides \
+		kernel-introduction-title \
+		sysdev-linux-intro-features \
+		kernel-intro-sources \
+		kernel-source-code-title \
+		kernel-source-code-drivers \
+		kernel-linux-usage-title \
+		sysdev-kernel-building \
+		sysdev-kernel-booting \
+		kernel-intro-modules
